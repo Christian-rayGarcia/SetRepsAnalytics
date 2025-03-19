7c3f7c8444d1f47f3b91956c466fd743486f92aa
Christian-Ray D. Garcia <47110238+Christian-rayGarcia@users.noreply.github.com>
2025-03-19 05:16:57 +0000

    Update main.py
    Function to show user record

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import json
 
 today_date = datetime.today().strftime('%d/%m/%Y')
 
-
 training_data = {
 }
 
@@ -28,12 +27,22 @@ def add_record(user_id, date, exercise, weight, reps):
         # If user doesn't exist, create a new list with the record
         training_data[user_id] = [new_record]
 
-    return f"Record added for user {user_id}"
+
+def show_records(user_id):
+    if user_id in training_data and training_data[user_id]:
+        print(f"\n Records for User {user_id}:")
+        for record in training_data[user_id]:
+            print(
+                f"Record ID: {record['record_id']}, Date: {record['date']}, Exercise: {record['exercise']}, Weight: {record['weight']}, Reps: {record['reps']} ")
+        else:
+            print(f"No records found for User {user_id}")
 
 
+"""
 add_record(101, "2025-03-17", "Deadlift", 180, 5)
 add_record(101, "2025-03-17", "sqaut", 180, 5)
 add_record(102, "2025-03-17", "Deadlift", 180, 5)
 
 formatted_users = json.dumps(training_data, indent=4)
 print(formatted_users)
+"""
